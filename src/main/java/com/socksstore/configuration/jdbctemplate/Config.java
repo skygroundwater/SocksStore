dc73d17bdc3fb1e707baacef44aeaf9203708ab6
Nitro <o.metelev2020@yandex.ru>
2023-02-14 21:07:18 +0300

    completed

--- src/main/java/com/socksstore/configuration/jdbctemplate/Config.java
+++ src/main/java/com/socksstore/configuration/jdbctemplate/Config.java
@@ -1,21 +1,32 @@
 package com.socksstore.configuration.jdbctemplate;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
+@PropertySource("classpath:database.properties")
 public class Config{
+
+    private final Environment environment;
+    @Autowired
+    public Config(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5433/socks_store");
-        dataSource.setPassword("Tilitilitatata12345");
-        dataSource.setUsername("postgres");
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setPassword(environment.getProperty("password"));
+        dataSource.setUsername(environment.getProperty("username"));
         return dataSource;
     }
 
