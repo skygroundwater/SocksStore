0dd72332ddca33438480effc837b7294a91b0972
Nitro <o.metelev2020@yandex.ru>
2023-02-14 20:13:18 +0300

    completed

--- src/main/java/com/socksstore/services/databaseservice/DataBaseService.java
+++ src/main/java/com/socksstore/services/databaseservice/DataBaseService.java
@@ -4,10 +4,13 @@ import com.socksstore.models.socks.prototype.SocksPrototype;
 
 import java.io.File;
 import java.sql.ResultSet;
+import java.util.List;
 
 public interface DataBaseService {
 
     File getTextFileWithSocks();
+
+    void deleteFromTheDataBase(SocksPrototype socksPrototype);
 
     void addQuantityForSocks(SocksPrototype socksPrototype, long quantity);
 
@@ -15,5 +18,5 @@ public interface DataBaseService {
 
     void insertToDatabase(SocksPrototype socksPrototype);
 
-    ResultSet selectFromDataBase(String color, Double size, Integer minComposition, Integer maxComposition);
+    List<SocksPrototype> selectFromDataBase(String color, Double size, Integer minComposition, Integer maxComposition);
 }
